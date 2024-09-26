019bb364c042d29f102b357b3ef706869d21648f
unknown <emiliojoaquinortizmalich@gmail.com>
2024-09-26 16:21:55 -0300

    ahora se pueden cambiar los datos y quedan almacenados en el local Storage

--- client/src/pages/Perfil.jsx
+++ client/src/pages/Perfil.jsx
@@ -7,16 +7,19 @@ export const Perfil = () => {
     return savedInfo
       ? JSON.parse(savedInfo)
       : {
-          direccion: "San Hilario, Formosa, Argentina",
-          telefono: "3704040642",
-          trabajo: "Desocupado",
-          cargo: "Ninguno",
-          email: "ejemplo@correo.com",
-          sitioWeb: "www.ejemplo.com",
-        };
+        nombre: "Emilio Joaquin Ortiz Malich",
+        descripcion:
+          "Soy un joven de 18 años con la intención de mostrar mi talento en mi deporte favorito. Me desempeño bien jugando al fútbol con mis compañeros. Espero poder encontrar a una persona que me ayude a explotar mi potencial.",
+        direccion: "San Hilario, Formosa, Argentina",
+        telefono: "3704040642",
+        trabajo: "Desocupado",
+        cargo: "Ninguno",
+        email: "ejemplo@correo.com",
+      };
   };
 
   const [info, setInfo] = useState(getInitialInfo);
+  const [isModalOpen, setIsModalOpen] = useState(false); // Estado para el modal
 
   // Manejar cambios en los inputs
   const handleChange = (e) => {
@@ -32,36 +35,17 @@ export const Perfil = () => {
     });
   };
 
-  // socialLinks no necesita cambios
-  const socialLinks = [
-    {
-      href: "https://www.facebook.com/emiliojoaquin.ortizmalich",
-      icon: "fab fa-facebook-f",
-      color: "text-blue-600",
-      hoverColor: "hover:text-blue-800",
-    },
-    {
-      href: "https://x.com/Emilio_joa_16",
-      icon: "fab fa-twitter",
-      color: "text-blue-500",
-      hoverColor: "hover:text-blue-700",
-    },
-    {
-      href: "https://www.instagram.com/emilio_joaquin_13/",
-      icon: "fab fa-instagram",
-      color: "text-pink-600",
-      hoverColor: "hover:text-pink-800",
-    },
-  ];
-
   return (
     <div className="container mx-auto p-4 mt-28">
       <section className="bg-white shadow-2xl rounded-lg overflow-hidden dark:bg-gray-900">
         {/* Header */}
         <div className="sticky">
           <div className="p-2 flex justify-center items-center -bottom-12">
-            <div className="w-24 h-24 rounded-full overflow-hidden shadow-lg dark:bg-white  border-2">
-              <img src="../assets/img/spot.png" alt="Foto de perfil de Emilio Joaquin Ortiz Malich" />
+            <div className="w-24 h-24 rounded-full overflow-hidden shadow-lg dark:bg-white border-2">
+              <img
+                src="../assets/img/spot.png"
+                alt={`Foto de perfil de ${info.nombre}`}
+              />
             </div>
           </div>
         </div>
@@ -70,13 +54,10 @@ export const Perfil = () => {
         <div className="mt-3 p-1 dark:bg-gray-900">
           <div className="text-center">
             <h3 className="text-2xl font-bold text-gray-800 dark:text-white">
-              Emilio Joaquin Ortiz Malich
+              {info.nombre}
             </h3>
             <p className="text-black mt-2 dark:text-white">
-              Soy un joven de 18 años con la intención de mostrar mi talento en
-              mi deporte favorito. Me desempeño bien jugando al fútbol con mis
-              compañeros. Espero poder encontrar a una persona que me ayude a
-              explotar mi potencial.
+              {info.descripcion}
             </p>
           </div>
 
@@ -103,94 +84,114 @@ export const Perfil = () => {
                 <i className="fas fa-envelope mr-2 text-purple-500"></i>
                 <span>Email: {info.email}</span>
               </li>
-              <li className="flex items-center">
-                <i className="fas fa-globe mr-2 text-indigo-500"></i>
-                <span>Sitio web: {info.sitioWeb}</span>
-              </li>
             </ul>
 
-            {/* Formulario para ingresar datos */}
             <div className="p-4">
-              <h2>Editar información</h2>
-              <form className="space-y-4">
-                <div>
-                  <label className="block">Dirección:</label>
-                  <input
-                    type="text"
-                    name="direccion"
-                    value={info.direccion}
-                    onChange={handleChange}
-                    className="w-full border rounded p-2"
-                  />
-                </div>
-                <div>
-                  <label className="block">Teléfono:</label>
-                  <input
-                    type="text"
-                    name="telefono"
-                    value={info.telefono}
-                    onChange={handleChange}
-                    className="w-full border rounded p-2"
-                  />
-                </div>
-                <div>
-                  <label className="block">Trabaja en:</label>
-                  <input
-                    type="text"
-                    name="trabajo"
-                    value={info.trabajo}
-                    onChange={handleChange}
-                    className="w-full border rounded p-2"
-                  />
-                </div>
-                <div>
-                  <label className="block">Cargo:</label>
-                  <input
-                    type="text"
-                    name="cargo"
-                    value={info.cargo}
-                    onChange={handleChange}
-                    className="w-full border rounded p-2"
-                  />
-                </div>
-                <div>
-                  <label className="block">Email:</label>
-                  <input
-                    type="email"
-                    name="email"
-                    value={info.email}
-                    onChange={handleChange}
-                    className="w-full border rounded p-2"
-                  />
-                </div>
-                <div>
-                  <label className="block">Sitio web:</label>
-                  <input
-                    type="text"
-                    name="sitioWeb"
-                    value={info.sitioWeb}
-                    onChange={handleChange}
-                    className="w-full border rounded p-2"
-                  />
-                </div>
-              </form>
+              <h2 className="dark:text-white">Editar información</h2>
+              {/* Botón para abrir el modal */}
+              <button
+                onClick={() => setIsModalOpen(true)}
+                className="bg-blue-500 text-white px-4 py-2 rounded">
+                Editar Perfil
+              </button>
             </div>
           </div>
 
-          {/* Social Media */}
-          <div className="mt-8 flex justify-center space-x-4">
-            {socialLinks.map((link, index) => (
-              <a
-                key={index}
-                href={link.href}
-                target="_blank"
-                rel="noreferrer"
-                className={`${link.color} ${link.hoverColor}`}
-              >
-                <i className={`${link.icon} text-2xl`}></i>
-              </a>
-            ))}
-          </div>
+          {/* Modal */}
+          {isModalOpen && (
+            <div className="absolute inset-2 bg-gray-600 bg-opacity-50  flex justify-center j items-center">
+              <div className="bg-white p-8 rounded-lg shadow-lg">
+                <h2 className="text-xl mb-4">Editar Perfil</h2>
+                <form className="space-y-2">
+                  <div>
+                    <label className="block">Nombre:</label>
+                    <input
+                      type="text"
+                      name="nombre"
+                      value={info.nombre}
+                      onChange={handleChange}
+                      className="w-full border rounded p-1"
+                    />
+                  </div>
+                  <div>
+                    <label className="block">Descripción:</label>
+                    <textarea
+                      name="descripcion"
+                      value={info.descripcion}
+                      onChange={handleChange}
+                      className="w-full border rounded p-1"
+                    ></textarea>
+                  </div>
+                  <div>
+                    <label className="block">Dirección:</label>
+                    <input
+                      type="text"
+                      name="direccion"
+                      value={info.direccion}
+                      onChange={handleChange}
+                      className="w-full border rounded p-1"
+                    />
+                  </div>
+                  <div>
+                    <label className="block">Teléfono:</label>
+                    <input
+                      type="text"
+                      name="telefono"
+                      value={info.telefono}
+                      onChange={handleChange}
+                      className="w-full border rounded p-1"
+                    />
+                  </div>
+                  <div>
+                    <label className="block">Trabaja en:</label>
+                    <input
+                      type="text"
+                      name="trabajo"
+                      value={info.trabajo}
+                      onChange={handleChange}
+                      className="w-full border rounded p-1"
+                    />
+                  </div>
+                  <div>
+                    <label className="block">Cargo:</label>
+                    <input
+                      type="text"
+                      name="cargo"
+                      value={info.cargo}
+                      onChange={handleChange}
+                      className="w-full border rounded p-2"
+                    />
+                  </div>
+                  <div>
+                    <label className="block">Email:</label>
+                    <input
+                      type="email"
+                      name="email"
+                      value={info.email}
+                      onChange={handleChange}
+                      className="w-full border rounded p-2"
+                    />
+                  </div>
+                </form>
+
+                {/* Botones para cerrar o guardar */}
+                <div className="mt-4 flex justify-end">
+                  <button
+                    onClick={() => setIsModalOpen(false)}
+                    className="bg-red-500 text-white px-4 py-2 rounded mr-2"
+                  >
+                    Cancelar
+                  </button>
+                  <button
+                    onClick={() => setIsModalOpen(false)}
+                    className="bg-green-500 text-white px-4 py-2 rounded"
+                  >
+                    Guardar
+                  </button>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       </section>
     </div>
