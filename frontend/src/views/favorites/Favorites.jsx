7c4c736396de6b629a9fcacc445443e270f755b0
unknown <emiliojoaquinortizmalich@gmail.com>
2024-10-29 12:35:32 -0300

    se agrego modo oscuro

--- frontend/src/views/favorites/Favorites.jsx
+++ frontend/src/views/favorites/Favorites.jsx
@@ -29,20 +29,21 @@ export const Favorites = () => {
   return (
     <>
       <Header />
-      <main className="w-full min-h-screen">
-        <ul className="w-full min-h-screen flex justify-center flex-col items-center pt-24">
+      <main className="w-full min-h-screen dark:bg-gray-800">
+        <ul className="w-full min-h-screen flex justify-center flex-col items-center pt-24 " >
           {favorites.length === 0 ? (
             <p>No tienes publicaciones favoritas.</p>
           ) : (
             favorites.map((favorite, index) => (
               <article key={index} className="border w-5/12 border-gray-300 dark:border-gray-700 rounded-lg p-4 mb-4 bg-white dark:bg-gray-800 shadow-md flex flex-col">
-                <p>{favorite.publicationId.author}</p>
-                <h3 className="break-words">{favorite.publicationId.title}</h3>
-                <p className="break-words">{favorite.publicationId.description}</p>
+                <p className="dark:text-white font-bold text-xl">{"@" + favorite.publicationId.author}</p>
+                <h3 className="break-words dark:text-white font-bold">{favorite.publicationId.title}</h3>
+                <p className="break-words ">{favorite.publicationId.description}</p>
+                <br />
                 <img
                   src={favorite.publicationId.secure_url}
                   alt={favorite.publicationId.title}
-                  className="favorite-image h-auto"
+                  className="favorite-image h-auto border border-gray-300 dark:border-gray-700 rounded-xl w-full"
                 />
               </article>
             ))
